856bf80933ae0c388cd970c8b8e0968e76e0f717
Lukaaas12 <lukasalvaradonava@gmail.com>
2026-05-13 09:21:13 -0400

    CRUD

--- frontend/src/app/features/landing/landing.component.ts
+++ frontend/src/app/features/landing/landing.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [RouterLink], // Esto permite que el botón viaje al login
+  imports: [RouterLink], // Asegúrate de que RouterLink esté aquí
   templateUrl: './landing.component.html'
 })
-export class LandingComponent { }
+export class LandingComponent {
+  private router = inject(Router);
+
+  // ESTA ES LA FUNCIÓN QUE FALTA:
+  cerrarSesion() {
+    localStorage.removeItem('sesion_activa');
+    this.router.navigate(['/login']);
+  }
+}
